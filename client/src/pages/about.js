a2a3b1aacaae051c91d085af37ddb5be97708ad8
KishaMims <86165064+KishaMims@users.noreply.github.com>
2022-04-08 15:00:27 -0500

    grammer correcrtion

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -7,7 +7,7 @@ function About() {
         <h1 className="HomeTitle">🍷🍹When it comes to a drink? I'm gonna have it🍷🍹</h1>
         <img alt="cockatials" src={image} height={400} width={600}/><br></br>
             <br></br>
-            <h3 className="HomeTitle">Making cocktails has been a at home hobby for me.. so I decided to share some of my favorite recipes here! </h3>
+            <h3 className="HomeTitle">Making cocktails has been an at home hobby for me.. so I decided to share some of my favorite recipes here! </h3>
     </div>
   )
 }
